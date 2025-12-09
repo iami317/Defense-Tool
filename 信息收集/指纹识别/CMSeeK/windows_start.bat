2a30061bb7e56594a5aaddf34018c43b048ecf09
iami317 <mlh_210@163.com>
2025-12-09 10:51:08 +0800

    指纹识别

---
+++ 信息收集/指纹识别/CMSeeK/windows_start.bat
@@ -0,0 +1,13 @@
+chcp 65001
+
+@echo off
+
+
+
+%~d0    %进入这个脚本执行的盘符%
+cd %~dp0   %进入这个脚本执行的目录%
+
+..\Python38\python.exe cmseek.py
+
+cmd
+
---
+++ 信息收集/指纹识别/Wafw00f/setup.bat
@@ -0,0 +1,10 @@
+chcp 65001
+
+@echo off
+
+%~d0    %进入这个脚本执行的盘符%
+cd %~dp0   %进入这个脚本执行的目录%
+
+..\Python311\python.exe setup.py install
+
+
