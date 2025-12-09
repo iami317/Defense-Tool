967de2d869c4eb1971a5670cf06c9db8981684b9
iami317 <mlh_210@163.com>
2025-12-09 10:56:49 +0800

    js收集

---
+++ 信息收集/JS收集/Rotor-Goddess/windows_start.bat
@@ -0,0 +1,18 @@
+chcp 65001
+
+@echo off
+
+
+%~d0 
+cd %~dp0 
+
+Rotor_Goddess.exe -h
+
+@echo.
+@echo.
+@echo.
+@echo.
+@echo "******************************************************
+@echo 使用:  Rotor_Goddess.exe 
+@echo "******************************************************
+cmd
